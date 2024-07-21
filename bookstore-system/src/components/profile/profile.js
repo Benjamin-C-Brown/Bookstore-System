67a087144e20f23ba259eed72fc7b361f6a26d9a
Benjamin Brown <benjaminbrownforte@gmail.com>
2024-07-21 19:04:14 -0400

    made profile fields editable aside from password (will eventually be connected to the database)

--- bookstore-system/src/components/profile/profile.js
+++ bookstore-system/src/components/profile/profile.js
@@ -2,6 +2,18 @@ import React from 'react';
 import './profile.css';
 import NavBar from '../navbar/NavBar.js';
 
+function editField(inputID, editID) {
+  const input = document.getElementById(inputID);
+  const edit = document.getElementById(editID);
+  if (edit.innerHTML == "(Edit)") {
+    edit.innerHTML = "(Save)";
+    input.contentEditable = true;
+  } else {
+    edit.innerHTML = "(Edit)";
+    input.contentEditable = false;
+  }
+}
+
 const Profile = () => {
   return (
     <div>
@@ -13,8 +25,8 @@ const Profile = () => {
               <div class="line">
                 <label class="label">Name:</label>
                 <span>
-                  <label class="value">Benjamin Brown</label>
-                  <label>(<a href="">Edit</a>)</label>
+                  <label id="name" class="value">FirstName LastName</label>
+                  <label id="nameEdit" class="edit" onClick={() => editField("name", "nameEdit")}>(Edit)</label>
                 </span>
               </div>
               <div class="line">
@@ -33,36 +45,48 @@ const Profile = () => {
             <div class="address">
               <div class="line">
                 <label class="label">Address:</label>
-                <label class="value">88, Street Avenue</label>
+                <span>
+                  <label id="address" class="value">88, Street Avenue</label>
+                  <label id="addressEdit" class="edit" onClick={() => editField("address", "addressEdit")}>(Edit)</label>
+                </span>
               </div>
               <div class="line">
                 <label></label>
-                <label class="value">Ringgold, GA 11111</label>
-              </div>
-              <div class="buttondiv">
-                <button>Edit address information</button>
+                <span>
+                  <label id="address2" class="value">Ringgold, GA 11111</label>
+                  <label id="address2Edit" class="edit" onClick={() => editField("address2", "address2Edit")}>(Edit)</label>
+                </span>
               </div>
             </div>
             <hr/>
             <div class="payment">
               <div class="line">
                 <label class="label">Credit card type:</label>
-                <label class="value">VISA</label>
+                <span>
+                  <label id="cardtype" class="value">VISA</label>
+                  <label id="cardtypeEdit" class="edit" onClick={() => editField("cardtype", "cardtypeEdit")}>(Edit)</label>
+                </span>
               </div>
               <div class="line">
                 <label class="label">Credit card number:</label>
-                <label class="value">1111-1111-1111-****</label>
+                <span>
+                  <label id="cardnumber" class="value">1111-1111-1111-****</label>
+                  <label id="cardnumberEdit" class="edit" onClick={() => editField("cardnumber", "cardnumberEdit")}>(Edit)</label>
+                </span>
               </div>
               <div class="line">
                 <label class="label">Expiration date:</label>
-                <label class="value">06/27</label>
+                <span>
+                  <label id="expiration" class="value">06/27</label>
+                  <label id="expirationEdit" class="edit" onClick={() => editField("expiration", "expirationEdit")}>(Edit)</label>
+                </span>
               </div>
               <div class="line">
                 <label class="label">CVV:</label>
-                <label class="value">***</label>
-              </div>
-              <div class="buttondiv">
-                <button>Edit payment information</button>
+                <span>
+                  <label id="cvv" class="value">***</label>
+                  <label id="cvvEdit" class="edit" onClick={() => editField("cvv", "cvvEdit")}>(Edit)</label>
+                </span>
               </div>
             </div>
           </div>
